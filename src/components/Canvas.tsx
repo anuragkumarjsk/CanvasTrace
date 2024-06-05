0555cadc3f5416ae367005fa6519289702260345
anuragkumarjsk <anuragkumarjsk13@gmail.com>
2024-06-05 18:27:21 +0530

    fix: modified configuration to enable deploy

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,4 +1,4 @@
-import React,{useRef,useState,useEffect} from 'react'
+import {useRef,useState,useEffect} from 'react'
 
 const Canvas = ({ imageUrl,windowWidth, windowHeight }) => {
     const canvasRef = useRef(null);
